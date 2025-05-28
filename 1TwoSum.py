168a5b3f0afdf36127b270142e1db68d2ca48616
SairamNagarajan1 <databytesairam@gmail.com>
2025-05-28 09:12:11 +0530

    Update 1TwoSum.py

--- 1TwoSum.py
+++ 1TwoSum.py
@@ -2,7 +2,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
       hashmap={}
-      for i,num in enumerate(len(nums)):
+      for i,num in enumerate(nums):
         complement=target-num
         if complement in hashmap:
           return[hashmap[complement],i]
